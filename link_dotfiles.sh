0b6ed2a4d4a77a591044d9b6c63b07ac1c8540a1
Dan Toms <danjtoms@protonmail.com>
2024-03-22 23:16:24 +0000

    Add ripgrep install

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syu
 sudo pacman -S yay
 yay -Syu
-yay -S spotify nvim lazygit kitty firefox ttf-firacode-nerd tmux
+yay -S spotify nvim lazygit kitty firefox ttf-firacode-nerd tmux ripgrep
 
 
 # Install Starship prompt
