c5ea06d0abb4b5f7746ffbb44292d65d2dc26a1c
Dan Toms <danjtoms@protonmail.com>
2024-03-22 22:54:57 +0000

    Add tmux config, symlink and install

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -4,10 +4,11 @@
 sudo pacman -Syu
 sudo pacman -S yay
 yay -Syu
-yay -S spotify nvim lazygit kitty firefox ttf-fira-code
+yay -S spotify nvim lazygit kitty firefox ttf-fira-code tmux
 
 
 
 # Symlinks
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
