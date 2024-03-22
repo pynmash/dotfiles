83b9ba5432bb46574779d2fc85923a1d7fb392e1
Dan Toms <danjtoms@protonmail.com>
2024-03-22 22:30:19 +0000

    Add package installation

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+#
+# Install these packages after system upgrade
+sudo pacman -Syu
+sudo pacman -S yay
+yay -Syu
+yay -S spotify nvim lazygit kitty
+
+
+
 # Symlinks
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
