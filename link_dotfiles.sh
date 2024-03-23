e522fb685f723b8b79e4c49c1c36f4f5b265ccac
Dan Toms <danjtoms@protonmail.com>
2024-03-23 19:18:32 +0000

    Add zsh install

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syu
 sudo pacman -S yay
 yay -Syu
-yay -S spotify nvim lazygit kitty firefox ttf-firacode-nerd tmux ripgrep dmenu
+yay -S spotify nvim lazygit kitty firefox ttf-firacode-nerd tmux ripgrep dmenu zsh
 
 
 # Install Starship prompt
