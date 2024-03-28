14954a0e2c8359f48fffbfff1684e387eaee64a8
Dan Toms <danjtoms@protonmail.com>
2024-03-28 23:06:12 +0000

    Add git set up and a bit of decoration

--- setup.sh
+++ setup.sh
@@ -1,15 +1,66 @@
 #!/bin/bash
 #
 # Install these packages after system upgrade
+
+echo "-----------------------------------------------"
+echo "Updating system and installing needed packages"
+echo "-----------------------------------------------"
 sudo pacman -Syu
 sudo pacman -S yay git
 yay -Syu
 yay -S spotify nvim lazygit kitty firefox ttf-firacode-nerd tmux ripgrep dmenu zsh eza fzf bat stow
 
 
+echo "---------------------------"
+echo "Installing Starship Prompt"
+echo "---------------------------"
 # Install Starship prompt
 curl -sS https://starship.rs/install.sh | sh
 
+
+echo "-----------------------"
+echo "Swtiching to ZSH shell"
+echo "-----------------------"
 # Switch shells
 chsh -s /usr/bin/zsh
 
+
+# Set up git
+echo "---------------"
+echo "Setting up Git"
+echo "---------------"
+
+# Set up user name and email
+echo "Set user.name:"
+read USER_NAME
+echo "Set user.email:"
+read USER_EMAIL
+git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
+
+# Set default branch name
+git config --global init.defaultBranch main
+
+# Enable colourful output
+git config --global init.defaultBranch main
+
+# No rebase
+git config --global pull.rebase false
+
+# Verify name and email
+git config --get user.name
+git config --get user.email
+
+# Create SSH key
+ssh-keygen -t ed25519 -C "$USER_EMAIL"
+SSH_KEY=$(cat ~/.ssh/id_ed25519.pub)
+echo "Add the following SSH Key to github.com and press ENTER to authenticate"
+echo "SSH Key: $SSH_KEY"
+read RETURN
+ssh -T git@github.com
+
+
+echo "-----"
+echo "Done"
+echo "-----"
+echo "It is recomended to log out and back in to source everthing correctly"
