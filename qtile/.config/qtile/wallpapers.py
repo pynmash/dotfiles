81196cdc5eaab083bc0038655e57e725abea310f
pynmash <danjtoms@protonmail.com>
2024-10-30 02:49:27 +0000

    Add get wallaper method

--- qtile/.config/qtile/wallpapers.py
+++ qtile/.config/qtile/wallpapers.py
@@ -6,3 +6,8 @@ class Wallpapers:
         self.path = paper_path
         self.current_wallpaper = ""
         
+    def get_wallpaper(self):
+        self.files = [file for file in listdir(self.path)]
+        self.current_wallpaper = choice(self.files)
+        
+        
