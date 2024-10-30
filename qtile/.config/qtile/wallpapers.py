e713dc0e954a6740c90673fa926bdb5064496094
pynmash <danjtoms@protonmail.com>
2024-10-30 22:26:29 +0000

    Add module to qtile

--- qtile/.config/qtile/wallpapers.py
+++ qtile/.config/qtile/wallpapers.py
@@ -3,21 +3,21 @@ from random import choice
 
 
 class Wallpapers:
-    def __init__(self, paper_path):
-        self.path = paper_path
-        self.current_wallpaper = ""
+    def __init__(self, paper_path: str):
+        self.path: str = paper_path
+        self.current_wallpaper: str = ""
         
     def get_wallpaper(self):
-        self.files = [file for file in listdir(self.path)]
-        self.current_wallpaper = choice(self.files)
+        self.files: list = [file for file in listdir(self.path)]
+        self.current_wallpaper: str = choice(self.files)
         
-    def set_wallapaper(self):
-        system(f"feh --bg-scale '/home/dan/wallapers/{self.current_wallpaper}'")
+    def set_wallpaper(self):
+        system(f"feh --bg-scale '/home/dan/wallpapers/{self.current_wallpaper}'")
         
 if __name__ == "__main__":
-    path = input("Enter the path to the folder where the wallpapers are stored: ")
+    path: str = input("Enter the path to the folder where the wallpapers are stored: ")
     if path == "":
-        path = "/home/dan/wallapers/"
+        path: str = "/home/dan/wallapers/"
     wallpapers = Wallpapers(paper_path=path)
     wallpapers.get_wallpaper()
     wallpapers.set_wallapaper()
