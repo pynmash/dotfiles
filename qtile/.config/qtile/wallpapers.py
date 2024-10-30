0eb952237bbb4071654fe1e398054e04d34fc64d
pynmash <danjtoms@protonmail.com>
2024-10-30 22:51:56 +0000

    Add docstring

--- qtile/.config/qtile/wallpapers.py
+++ qtile/.config/qtile/wallpapers.py
@@ -1,8 +1,16 @@
+"""
+    A qtile module to change to a random wallpaper each time the configuration is loaded, mostly on login.
+
+    Requires 'feh' to be installed as this is the shell program used to set the wallpaper.
+"""
+
 from os import listdir, system
 from random import choice
 
 
 class Wallpapers:
+    """
+    """
     def __init__(self, paper_path: str):
         self.path: str = paper_path
         self.current_wallpaper: str = ""
